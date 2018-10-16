4972104f2dfc3b7b755f8e475b1479479cd12df0
johnshen <johnshencanada@gmail.com>
2018-10-16 11:53:51 -0700

    space

--- src/reducers/player.js
+++ src/reducers/player.js
@@ -12,36 +12,3 @@ const initialState = [
     score: 50,
   },
 ];
-
-export default function Player(state=initialState,action) {
-	switch(action.type) {
-    case 'ADD_PLAYER':
-			return [
-				...state,
-				{
-					name: action.name,
-					score: 0,
-				}
-			];
-		case 'REMOVE_PLAYER':
-			return [
-				...state.slice(0, action.index),
-				...state.slice(action.index+1)
-			];
-
-		case 'UPDATE_PLAYER_SCORE':
-			return state.map((player,index) => {
-				if(index === action.index) {
-					return {
-						...player,
-						score:player.score + action.score
-					}
-				}
-				return player;
-			});
-
-	  default:
-	    return state;
-	}
-}
-
